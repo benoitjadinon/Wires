4fea5807cdfd9e3ffec6beec7818fa11e7c6c7d7
Benoit Jadinon <j@dinonbeno.it>
2017-02-20 18:53:29 +0100

    Added sample Droid project

--- Sources/Wires.Sample.Droid/MainActivity.cs
+++ Sources/Wires.Sample.Droid/MainActivity.cs
@@ -1,26 +1,69 @@
-﻿using Android.App;
+using Android.App;
 using Android.Widget;
 using Android.OS;
+using Wires;
+using Wires.Sample.ViewModel;
+using Java.Lang.Reflect;
 
 namespace Wires.Sample.Droid
 {
-	[Activity(Label = "Wires", MainLauncher = true, Icon = "@mipmap/icon")]
+	[Activity(Label = "Wires.Sample.Droid", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		HomeViewModel ViewModel { get; set; }
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
+								
+			this.ViewModel = new HomeViewModel();
 
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
+		}
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+		TextView label;
+		EditText field;
+		Button button;
 
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+		public override Android.Views.View OnCreateView(string name, Android.Content.Context context, Android.Util.IAttributeSet attrs)
+		{
+			label = FindViewById<TextView>(Resource.Id.textView1);
+			field = FindViewById<EditText>(Resource.Id.editText1);
+			button = FindViewById<Button>(Resource.Id.button1);
+
+			this.ViewModel
+				.Bind(this.label)
+					.Text(vm => vm.Title, Converters.Uppercase)
+				.Bind(this.field)
+					.Text(vm => vm.Title)
+				/*
+			    .Bind(this.textView)
+					.Text(vm => vm.Title)
+					.UserInteractionEnabled(vm => vm.IsActive)
+				.Bind(this.image)
+					.ImageAsync(vm => vm.Illustration)
+					.Alpha(vm => vm.Amount)
+					.Visible(vm => vm.IsActive)
+				.Bind(this.toggleSwitch)
+					.On(vm => vm.IsActive)
+				.Bind(this.slider)
+					.Value(vm => vm.Amount)
+				.Bind(this.datePicker)
+					.Date(vm => vm.Birthday)
+				.Bind(this.progressView)
+					.Progress(vm => vm.Amount)
+				.Bind(this.activityIndicator)
+					.IsAnimating(vm => vm.IsLoading)
+				.Bind(this.segmented)
+					.Titles(vm => vm.Sections)
+					.Selected(vm => vm.Selected)
+			    */
+				.Bind(this.button)
+					.TouchUpInside(vm => vm.LoadCommand)
+			    ;
+
+			return base.OnCreateView(name, context, attrs);
 		}
 	}
 }
